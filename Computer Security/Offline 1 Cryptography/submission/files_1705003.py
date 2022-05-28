6418cf1f3144a8f528141a155201f5387dd85049
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-05-28 08:47:51 +0600

    Added diir creation

--- Computer Security/Offline 1 Cryptography/submission/files_1705003.py
+++ Computer Security/Offline 1 Cryptography/submission/files_1705003.py
@@ -1,3 +1,7 @@
 import os
 def file_path(relative_path):
-	return os.path.join(os.path.dirname(__file__), relative_path)
+	file = os.path.join(os.path.dirname(__file__), relative_path)
+	# create directory if not exists
+	if not os.path.exists(os.path.dirname(file)):
+		os.makedirs(os.path.dirname(file))
+	return file
