9d01bb94ae5d9efc14d2506867884868e3420f66
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-06-19 21:44:57 +0600

    added b1 online

---
+++ Computer Security/Buffer Overflow Onlines/Online 1 17-B1/Online 1 B1/tmp.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int b = 0x111111;
+    int a = 21 - b;
+    unsigned int x = a;
+    cout<<" signed a = "<<a<<"\n";
+    cout<<" unsigned a = "<<x<<"\n";
+    cout<<" b = "<<b<<"\n";
+}
