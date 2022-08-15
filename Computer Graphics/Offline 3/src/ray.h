b96a994e7f05ab040b8a8ae831308fbe3b350a52
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-15 21:02:52 +0600

    added ray cls

---
+++ Computer Graphics/Offline 3/src/ray.h
@@ -0,0 +1,15 @@
+#ifndef D46B3C26_2DFF_4FE7_8F21_2ECB686E278C
+#define D46B3C26_2DFF_4FE7_8F21_2ECB686E278C
+
+#include<vec3.h>
+
+template<typename T>
+class Ray
+{
+	Vec3<T> origin;
+	Vec3<T> direction;
+public:
+	
+};
+
+#endif /* D46B3C26_2DFF_4FE7_8F21_2ECB686E278C */
