4ac8619181313bebd9327cea79542adfa3bc490d
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-16 21:01:28 +0600

    initial triangle intersect

--- Computer Graphics/Offline 3/src/triangle.h
+++ Computer Graphics/Offline 3/src/triangle.h
@@ -18,6 +18,43 @@ public:
 		}
 		glEnd();
 	}
+
+	virtual T intersect(Ray<T> const& ray, Color<T> &color, int level)
+	{
+		Vec3<T> normal = (v[1]-v[0]).cross(v[2]-v[0]);
+		T tMin = (v[0]-ray.getOrigin()).dot(normal)/ray.getDirection().dot(normal);
+
+		Vec3<T> point = ray.getOrigin()+ray.getDirection()*tMin;
+		// check if point is withing triangle
+		Vec3<T> mn = v[0];
+		Vec3<T> mx = v[0];
+		for(int i=1;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+				mn[j]=min(mn[j],v[i][j]),
+				mx[j]=max(mx[j],v[i][j]);
+		}
+		bool withinTriangle = true;
+		for(int i=0;i<3;i++)
+		{
+			withinTriangle = withinTriangle and ((mn[i]<=point[i]) and (point[i]<=mx[i]));
+		}
+		if(withinTriangle)
+		{
+			// assert(0);
+		}
+		if(!withinTriangle)
+			tMin = T(-1);
+		
+
+		if(level == 0)
+			return tMin;
+		
+		DBG("Not implemented");
+		assert(0);
+		return T(-1);
+	}
+
 	virtual istream & read(istream & is)
 	{
 		is>>v[0]>>v[1]>>v[2];
