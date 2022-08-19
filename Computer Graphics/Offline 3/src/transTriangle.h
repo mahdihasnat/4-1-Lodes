2408be098d06b1a811db8104b9209f8fc5490804
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-20 05:42:47 +0600

    added check on both side

--- Computer Graphics/Offline 3/src/transTriangle.h
+++ Computer Graphics/Offline 3/src/transTriangle.h
@@ -4,33 +4,40 @@
 #include "triangle.h"
 #include "transObject.h"
 
-template<typename T>
+template <typename T>
 class TransTriangle : public Triangle<T>, public TransObject<T>
 {
 	T refractionCoefficient = 1;
+
 public:
 	virtual bool isTransparent() override
 	{
 		return true;
 	}
-	virtual Vec3<T> getNormalAt(Vec3<T> const& point) override
+	virtual Vec3<T> getNormalAt(Vec3<T> const &point) override
 	{
 		return Triangle<T>::getNormalAt(point);
 	}
-	virtual T getItaTOverItaI(Vec3<T> const& transmittedRay) override
+	virtual T getItaTOverItaI(Vec3<T> const &transmittedRay) override
 	{
 		// TODO : implement carefully
-		return refractionCoefficient;
+		Vec3<T> myNormal = (Triangle<T>::v[1] - Triangle<T>::v[0])
+							   .cross(Triangle<T>::v[2] - Triangle<T>::v[0]);
+		myNormal.normalize();
+		if (myNormal.dot(transmittedRay) > 0)
+			return refractionCoefficient;
+		else
+			return 1 / refractionCoefficient;
 	}
 	void setRefrectionCoefficient(T refractionCoefficient)
 	{
 		this->refractionCoefficient = refractionCoefficient;
 	}
-	virtual ostream & write(ostream & os) const override
+	virtual ostream &write(ostream &os) const override
 	{
-		os<<"[tt: [rc:"<<refractionCoefficient<<"] ";
+		os << "[tt: [rc:" << refractionCoefficient << "] ";
 		Triangle<T>::write(os);
-		os<<"]";
+		os << "]";
 		return os;
 	}
 };
